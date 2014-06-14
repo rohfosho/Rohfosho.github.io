57867db9990ee3015a2c2c7bdbdaf1bc44d1c5ba
Rohfosho <rohvar98@gmail.com>
2014-06-14 00:38:38 -0400

    Added KloudlessKontacts

---
+++ KloudlessKontacts/js/scripts.js
@@ -0,0 +1,123 @@
+// toggle visibility for css3 animations 
+$(document).ready(function() {
+	$('.payoff h1').addClass('visibility');
+	$('.features .col-md-4').addClass('visibility');
+});
+
+
+// Fixed navbar
+$(window).scroll(function () {
+
+var scrollTop = $(window).scrollTop();
+
+	if (scrollTop > 200) {
+		$('.navbar-default').css('display', 'block');
+		$('.navbar-default').addClass('fixed-to-top');
+			
+	} else if (scrollTop == 0)   {
+	
+		$('.navbar-default').removeClass('fixed-to-top');
+	}
+	
+	
+//animations	
+	$('.payoff h1').each(function(){
+			
+		var imagePos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+			
+			if (imagePos < topOfWindow+650) {
+				$(this).addClass("animated fadeInLeft");
+			}		
+				
+	});
+	
+	$('.purchase button.app-store').each(function(){
+			
+		var imagePos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+			
+			if (imagePos < topOfWindow+650) {
+				$(this).addClass("animated pulse");
+			}		
+				
+	});
+	
+	$('.features .col-md-4').each(function(){
+			
+		var imagePos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+			
+			if (imagePos < topOfWindow+650) {
+				$(this).addClass("animated pulse");
+			}		
+				
+	});
+	
+});
+
+
+// Parallax Content
+
+function parallax() {
+
+		// Turn parallax scrolling off for iOS devices
+		   
+		    var iOS = false,
+		        p = navigator.platform;
+		
+		    if (p === 'iPad' || p === 'iPhone' || p === 'iPod') {
+		        iOS = true;
+		    }
+	
+		var scaleBg = -$(window).scrollTop() / 3;
+
+        if (iOS === false) {
+            $('.payoff').css('background-position-y', scaleBg - 150);
+            $('.social').css('background-position-y', scaleBg + 200);
+        }
+   
+}
+
+$(document).ready(function () {
+
+	var browserWidth = $(window).width();
+	
+	if (browserWidth > 560){ 
+	
+		$(window).scroll(function() {
+			parallax();
+			navbar();
+		});
+	
+	}
+
+});	
+
+
+$(window).resize(function () {
+
+	var browserWidth = $(window).width();
+	
+	if (browserWidth > 560){ 
+	
+		$(window).scroll(function() {
+			parallax();
+			navbar();
+		});
+	
+	}
+
+});	
+
+
+// iPhone Header Carousel
+$('header .carousel').carousel({
+  interval: 3000
+})
+
+// iPhone Features Carousel
+$('.detail .carousel').carousel({
+  interval: 4000
+})
+
